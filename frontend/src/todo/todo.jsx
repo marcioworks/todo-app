c317908e2fbe95f1095d911ba07ef1f6b2b4be5e
Marcio Silva <marcioadsworks@gmail.com>
2020-01-25 20:33:48 -0300

    inserting not working

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,21 +1,28 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
+
 import PageHeader from '../templates/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
+
+URL = 'http://localhost:3003/api/todos'
 export default class Todo extends Component {
 
     constructor(props) {
         super(props)
-        this.state={description:'',list:[]}
+        this.state = { description: '', list: [] }
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
     }
     handleChange(e) {
-        this.setState({...this.state,description: e.target.value})
+        this.setState({ ...this.state, description: e.target.value })
     }
     handleAdd() {
-        console.log(this.state.description)
+        const description = this.state.description
+        axios.post(URL, { description })
+            .then(resp => console.log('funcionou!'))
     }
 
 
@@ -24,9 +31,9 @@ export default class Todo extends Component {
         return (
             <div>
                 <PageHeader name="Tarefas" small='Cadastro' />
-                <TodoForm description={this.state.description} 
-                handleAdd={this.handleAdd} 
-                handleChange={this.handleChange}/>
+                <TodoForm description={this.state.description}
+                    handleAdd={this.handleAdd}
+                    handleChange={this.handleChange} />
                 <TodoList />
             </div>
         )
