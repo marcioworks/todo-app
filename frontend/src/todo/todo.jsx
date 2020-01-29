247001edc6bc1c6b10601feef71ecd03de5dca8e
Marcio Silva <marcioadsworks@gmail.com>
2020-01-28 21:13:17 -0300

    delete button and remove from list

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -15,14 +15,28 @@ export default class Todo extends Component {
         this.state = { description: '', list: [] }
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+        this.refresh()
     }
+
+    refresh() {
+        axios.get(`${URL}?sort=createdAt`)
+            .then(resp => this.setState({ ...this.state, description: '', list: resp.data }))
+    }
+
     handleChange(e) {
         this.setState({ ...this.state, description: e.target.value })
     }
+
     handleAdd() {
         const description = this.state.description
         axios.post(URL, { description })
-            .then(resp => console.log('funcionou!'))
+            .then(resp => this.refresh())
+    }
+
+    handleRemove(todo){
+        axios.delete(`${URL}/${todo._id}`)
+                .then(resp => this.refresh())
     }
 
 
@@ -34,7 +48,7 @@ export default class Todo extends Component {
                 <TodoForm description={this.state.description}
                     handleAdd={this.handleAdd}
                     handleChange={this.handleChange} />
-                <TodoList />
+                <TodoList list={this.state.list} handleRemove={this.handleRemove}/>
             </div>
         )
     }
